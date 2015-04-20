ec01f9a88260ebbf6a1b6cf063ee4f963fd2ba2d
unknown <malbrech@L0502491.NMDP.ORG>
2015-04-20 14:55:47 -0500

    at the point of adding in a nested data table

--- shiny/server.R
+++ shiny/server.R
@@ -11,27 +11,31 @@ server <- function(input, output, session) {
   
   output$sampleTable <- renderDataTable({
     
-    DT::datatable(iris, callback = JS("table.on('mouseenter','tr',function (event) {",
-                                               "var check = $('td:eq(1)',this);",
-                                               "console.log(check);",
-                                               "$(this).qtip({",
-                                               "overwrite: false,",
-                                               "content: 'Hello',",
-                                               "position: {",
-                                               "my: 'right center',",
-                                               "at: 'left center',",
-                                               "target: $('td:eq(1)', this)",
-                                               "},",
-                                               "show: {",
-                                               "event: event.type,",
-                                               "ready: true",
-                                               "},",
-                                               "hide: {",
-                                               "fixed: true",
-                                               "}",
-                                               "}, event);",
-                                               "});")                    
-                                      )
+    ####specify the jquery eq to be equal to the table row element (starts at zero)
+    
+    DT::datatable(iris, callback = JS("table.on('mouseover','td',function (event) {",
+                                               "var colIdx = table.cell(this).index().column;",
+                                               "console.log(colIdx);",
+                                        "if ( colIdx === 3 ) {",
+                                        "$(this).qtip({",
+                                        "overwrite: false,",
+                                        "content: ,",
+                                        "position: {",
+                                        "my: 'right center',",
+                                        "at: 'left center',",
+                                        "target: this",
+                                        "},",
+                                        "show: {",
+                                        "event: event.type,",
+                                        "ready: true",
+                                        "},",
+                                        "hide: {",
+                                        "fixed: true",
+                                        "}",
+                                        "}, event);",
+                                        "}",
+                                        "})")
+    )
   })
   
 
